5f35611cb7a13c6dc4c727a5c8429e1b0b2d86c1
Jitendra Chaudhari <jitendra.chaudhari@siemens.com>
2017-10-02 18:18:26 +0530

    added routes

--- public/app.js
+++ public/app.js
@@ -1,7 +1,24 @@
 (function(){
 
 //Angular app  initiatalized with no dependencies
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ui.router']);
+
+app.config(function($stateProvider,$urlRouterProvider){
+  $stateProvider
+  .state({
+    name:'home',
+    url: '/home',
+    templateUrl:'/home.html'
+  })
+  .state(
+    {
+      name: 'about',
+      url:'/about',
+      template: '<h3>Its the UI-Router hello world app!</h3>'
+    }
+  );
+  $urlRouterProvider.otherwise('home');
+});
 
 app.controller("MainCntrl", ['$scope','Members', function($scope,Members){
   this.title = "This is my Startup App";
--- server.js
+++ server.js
@@ -4,8 +4,11 @@ var app = express();
 var server = http.createServer(app);
 var path =require('path');
 var io = require('socket.io')(server);
+var bodyParser = require ('body-parser');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}))
 app.set('views',path.join(__dirname+'views'));
 app.set('view engine','ejs');
 
@@ -13,9 +16,15 @@ app.use('/members',require('./routes/members.js'));
 io.on('connection', function(client){
   console.log("client connect through web socket");
   client.emit('greet',"Hey Hi I have been logged in.");
+  client.on('disconnect', function(){
+    console.log("Client disconnect");
+  })
 });
 
-//catch 404 and forward to error handler
+io.on('disconnect', function(){
+  console.log("scoket disconnect");
+})
+//catch 404 and forward to error handler and this should be always at bottom of stack
 app.use(function(request,response,next){
  var err = new Error("Not Found");
  err.status = 404;
